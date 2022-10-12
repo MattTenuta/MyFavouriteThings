6bac0c9ff7b85ed17e89f14414b5c3d61bde93c2
Tenuta <m_tenuta@ATS-HR420-DM09.fc.ca>
2022-10-12 18:02:41 -0400

    didnt work

--- js/main.js
+++ js/main.js
@@ -10,18 +10,26 @@ import { getData } from "./modules/dataMiner.js";
         lightbox = document.querySelector("#lightbox"),
         cookImg = document.querySelector("#cookingImg"),
         vacaImg = document.querySelector("#travelImg"),
+        mainImages = document.querySelector("#whateverWant"),
         gamerImg = document.querySelector("#gameImg");
 
     function changeCopy(hobbies) {
 
         let theHobbies = Object.keys(hobbies);
+        faveData = hobbies;
         
         theHobbies.forEach(hobby =>{
 
             let panel = theTemplate.cloneNode(true),
-                containers = panel.firstElementChild.children;
+                containers = panel.firstElementChild.children,
+                theMainImg = containers[0].querySelector('img');
 
-                containers[0].querySelector('img').src = `images/${hobbies[hobby].hobbyPic}`;
+                theMainImg.src = `images/${hobbies[hobby].activeThing}`;
+                theMainImg.dataset.key = hobby;
+
+                theMainImg.addEventListener('click', function(hobby) {
+                    createLightBoxContent(hobbies[hobby.target,dataset.key]);
+                })
 
                 containers[1].textContent = hobbies[hobby].title;
                 containers[2].textContent = hobbies[hobby].subheading;
@@ -33,23 +41,39 @@ import { getData } from "./modules/dataMiner.js";
         })
     }
 
+    function createLightBoxContent(hobby) {
+        debugger;
+
+        activeThing = faveData[this.id];
+
+        //retrieve the lightbox, change its content with data, show it
+
+        let lightbox = document.querySelector('#lightbox');
+        lightbox.classList.add('show-lightbox');
+
+        lightbox.querySelector('img').src = hobby.hobbyPic
+    }
+
     function childChecker() {
         if(lightbox.classList > 1) {
             lightbox.classList.remove("show-lightbox");
         }
     }
 
+
     function removeLightbox() {
         lightbox.classList.remove("show-lightbox");
     }
 
-    function showBox() {
+    function showBox(hobby) {
         childChecker();
 
         lightbox.classList.add("show-lightbox");
 
         lightbox.addEventListener("click", removeLightbox);
     }
+
+    mainImages.forEach(image => image.addEventListener('click', createLightBoxContent));
 
 
     getData('./data.json', changeCopy);
